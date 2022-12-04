39cb5709d173c2eb4bdff9611d4ae9dbbe3198c5
syRobin <sy759770423@163.com>
2022-12-04 22:11:30 +0800

    style: 调整代码结构

--- src/main/java/com/syrobin/me/hybrid/controller/HybirdController.java
+++ src/main/java/com/syrobin/me/hybrid/controller/HybirdController.java
@@ -17,13 +17,15 @@ public class HybirdController {
 
     @GetMapping("/hello")
     public String hello() {
+        return "hello";
+    }
 
+    public static void main(String[] args) {
         // Java代码作为启动代码
         System.out.println("Hello GraalVM! from Java");
         System.out.println("Let's go with JavaScript");
 
         Context polyglot = Context.create();
         polyglot.eval("js", "print('Hello GraalVM! from JavaScript')");
-        return "hello";
     }
 }
